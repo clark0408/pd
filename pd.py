fb9b02abf8e966d4a8c058a2a7419a832d0bc205
clark0408 <clark910408@gmail.com>
2022-04-25 11:59:40 +0800

    new

--- pd.py
+++ pd.py
@@ -4,6 +4,15 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
+	price = int(price)
 	p = []
 	produsts.append([name, price])
 print(produsts)
+
+for p in produsts
+	print(p[0],'的價格是',p[1])
+
+with open('produsts.csv','w', encoding='utf-8') as f:
+	f.write('商品,價格')
+	for p in produsts:
+		f.write(p[0]+ ','+ p[1]+'\n')
